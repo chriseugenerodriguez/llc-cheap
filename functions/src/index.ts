cd8ac77c3333de39aa8c2121700b1968287c12e9
zombie <mianhusnaingee@gmail.com>
2023-02-06 11:31:54 +0500

    setup firebase, auth function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,42 +1,33 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import * as cors from "cors";
-import * as firebaseAccountCredentials from "./config/index.json";
+import * as Credentials from "./config/index.json";
+import { getAuthorizationToken } from "./legalinc/authorization/getAuthorizationToken";
+import * as firebaseServiceAccount from "./config/llc-cheap-service-account.json";
 
+const serviceAccount = firebaseServiceAccount as admin.ServiceAccount;
 
-import * as getAuthorizationToken from './legalinc/authorization/getAuthorizationToken';
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-admin.initializeApp(functions.config().firebase);
+export const firestoreDB = admin.firestore();
 
-const serviceAccount = firebaseAccountCredentials.firebase as admin.ServiceAccount;
+const whitelist = ["http://localhost:4200", `${Credentials.legalinc.key.api}`];
 
-admin.initializeApp(
-  { credential: admin.credential.cert(serviceAccount) }
-);
-
-export const firestore = admin.firestore()
-
-
-const whitelist = [
-  'http://localhost:4200',
-  `${firebaseAccountCredentials.legalinc.key.api}`
-];
-
-const corsHandler = cors
-  ({
-
+const corsHandler = cors({
     origin: (origin: any, callback: any) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error(`Origin: ${origin} not allowed by CORS!!`))
-        // callback(null, true)
-      }
-    }
-  });
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error(`Origin: ${origin} not allowed by CORS!!`));
+            // callback(null, true)
+        }
+    },
+});
 
 module.exports = {
-  'getAuthorizationToken': functions.https.onRequest((req: functions.https.Request, res: functions.Response<any>) => {
-    corsHandler(req, res, async () => await getAuthorizationToken(req, res))
-  }),
-}
+    getAuthorizationToken: functions.https.onRequest(
+        (req: functions.https.Request, res: functions.Response<any>) => {
+            corsHandler(req, res, async () => await getAuthorizationToken(req, res));
+        }
+    ),
+};
--- functions/src/legalinc/authorization/getAuthorizationToken.ts
+++ functions/src/legalinc/authorization/getAuthorizationToken.ts
@@ -1,51 +1,24 @@
-import * as functions from "firebase-functions"
-import * as firebase from "firebase-admin"
-import { firestore } from "../index"
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { firestoreDB } from "../../index";
+import axios from "axios";
+import * as Credentials from "../../config/index.json";
 
 export async function getAuthorizationToken(
-  req: functions.https.Request,
-  res: functions.Response<any>
+    req: functions.https.Request,
+    res: functions.Response<any>
 ) {
-  console.log("Create Client Called")
+    try {
+        const extractData = await axios.post(
+            Credentials.legalinc.key.api + `oauth/${Credentials.legalinc.key.secret}`,
+            Credentials.legalinc.credentials
+        );
 
-  try {
-    const email = req.body.email
+        console.log("extractData ", extractData);
 
-    const clientQS = await firestore
-      .collection("Clients")
-      .where("email", "==", email)
-      .get()
-
-    const clientData = clientQS.docs
-
-    if (clientData.length > 0) {
-      return res.status(403).json({
-        message: "Client with email " + email + " already exists",
-      })
+        return res.status(201).json(extractData);
+    } catch (error) {
+        console.log("error ", error);
+        return res.status(403).json(error);
     }
-
-    const clientRecord = await firebase.auth().createUser({
-      email: email,
-      password: req.body.password,
-    })
-
-    console.log("Successfully created new client:", email)
-
-    await firestore.collection("Clients").doc(clientRecord.uid).set({
-      userType: "client",
-      name: req.body.name,
-      address: req.body.address,
-      id: clientRecord.uid,
-      email: email,
-      telephoneNumber: req.body.telephoneNumber,
-      // ordersHistory: null,
-    })
-
-    const data = await firestore.collection("Clients").doc(clientRecord.uid).get()
-    return res.status(201).json(data.data())
-
-  } catch (error) {
-    console.log("error ", error)
-    return res.status(403).json(error)
-  }
 }
