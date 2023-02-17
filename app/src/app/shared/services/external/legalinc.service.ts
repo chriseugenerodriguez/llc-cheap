9af1ef49975d7bab2fca33c4316de10527aedceb
cryptodev92 <dream.dev1992@gmail.com>
2023-02-17 14:58:07 +0200

    Revert "fix: make llc-cheap app to run as ssr service using angular universal"
    This reverts commit 3b18c0b3c81597bbaa9c3cd2ed2a44d0b535b864.

--- app/src/app/shared/services/external/legalinc.service.ts
+++ app/src/app/shared/services/external/legalinc.service.ts
@@ -41,6 +41,7 @@ export class LegalIncService {
     let eName = name;
     let eType = type;
     let eState = state;
+
     this._http.get(environment.legalinc.api + '/api/v2/entity/check', this.options).subscribe((res) => {
 
     });
